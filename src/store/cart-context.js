8a3c73621876d2b3f927fbf8380526d12cb7b32d
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-09-07 16:03:25 -0300

    feat: create cart context

---
+++ src/store/cart-context.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const CartContext = React.createContext({
+	items: [],
+	totalAmount: 0,
+	addItem: item => {},
+});
+
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+export function CartProvider({ children }) {
+	const cartLocalStorageValue = JSON.parse(localStorage.getItem("cart"));
+
+	const [cart, setCart] = useState(cartLocalStorageValue ?? defaultCartState);
+
+	function addItemToCartHandler(item) {
+		const updatedTotalAmount = cart.totalAmount + item.price;
+
+		const existingItemIndex = cart.items.findIndex(cartItem => cartItem.id === item.id);
+		const existingItem = cart.items[existingItemIndex];
+
+		let updatedItems;
+
+		if (existingItem) {
+			const updatedItem = { ...existingItem, amount: existingItem.amount + 1 };
+
+			updatedItems = [...cart.items];
+			updatedItems[existingItemIndex] = updatedItem;
+		} else {
+			updatedItems = cart.items.concat({ ...item, amount: 1 });
+		}
+
+		localStorage.setItem(
+			"cart",
+			JSON.stringify({
+				items: updatedItems,
+				totalAmount: updatedTotalAmount,
+			})
+		);
+
+		setCart({
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		});
+	}
+
+	const cartContext = {
+		items: cart.items,
+		totalAmount: cart.totalAmount,
+		addItem: addItemToCartHandler,
+	};
+	return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
+}
+
+export default CartContext;
