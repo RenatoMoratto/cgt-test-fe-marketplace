07d571d93682bfd02429d78f82164431adfa573a
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-09-07 21:55:40 -0300

    feat: styles product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import CartContext from "../store/cart-context";
 import { getProductById } from "../api";
+import styles from "./Product.module.css";
 
 function Product() {
 	const { addItem } = useContext(CartContext);
@@ -24,21 +25,29 @@ function Product() {
 	}, [id]);
 
 	return (
-		<div>
+		<>
 			{isLoading && <p>Loading...</p>}
 			{!isLoading && (
-				<>
-					<h1>{product.name}</h1>
-					<p>Price: {product.price} USD</p>
-
-					<button onClick={addToCartHandler}>Add to cart</button>
-
-					<div>
-						<img src={product.imageURL} width={640} alt="" />
+				<div className={styles.layout}>
+					<div className={styles.image}>
+						<img src={product.imageURL} alt="" />
 					</div>
-				</>
+					<div className={styles.card}>
+						<div className={styles.description}>
+							<h1>{product.name}</h1>
+							<p>{product.description}</p>
+						</div>
+
+						<div className={styles.price}>
+							<p>{product.price.toFixed(2)} USD</p>
+							<button className={styles.button} onClick={addToCartHandler}>
+								Add to cart
+							</button>
+						</div>
+					</div>
+				</div>
 			)}
-		</div>
+		</>
 	);
 }
 
