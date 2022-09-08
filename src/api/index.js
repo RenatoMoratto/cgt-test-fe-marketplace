034666f8324ce26036c4dfcf80ece9bb789bfe2c
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-09-08 12:15:48 -0300

    feat: create user interests and newest products mock api

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,7 @@ const PRODUCTS = [
 		description:
 			"3D Advanced crew escape suit astronaut modeled in high precision. Used in the space shuttle nasa program ( atlantis, endeavor, dicovery, challenger, columbia, enterprise ) this is a mix between several references i found. this model has been accurately recreated in 3d High poly to keep every details.",
 		imageURL: require("./imgs/a.jpg"),
+		userInterest: true,
 	},
 	{
 		id: "b",
@@ -14,6 +15,7 @@ const PRODUCTS = [
 		description:
 			"This is fully detailed statue of an Alien creature. This figure is prepared to be printed on SLA and also FDM printers. It's assmebled from 20 pieces - cut , keyed and hollowed. Separate Folder called FDM is a model with no holes, for better printing results. Model on the pictures was printed on the form2 SLA printer. You will find here files in formats like: stl, obj, FBX. This is only printable file and doesn't contain textures neither mtl.",
 		imageURL: require("./imgs/b.jpg"),
+		newests: true,
 	},
 ];
 
@@ -21,9 +23,14 @@ function simulateDelay() {
 	setInterval(() => {}, 400);
 }
 
-export async function getAllProducts() {
+export async function getUserInterestProducts() {
 	simulateDelay();
-	return PRODUCTS;
+	return PRODUCTS.filter(product => product.userInterest);
+}
+
+export async function getNewestProducts() {
+	simulateDelay();
+	return PRODUCTS.filter(product => product.newests);
 }
 
 export async function getProductById(id) {
