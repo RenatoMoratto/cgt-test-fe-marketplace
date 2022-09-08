c8e64c383fb6e726ee03497032d32fda1e982f17
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-09-08 13:02:36 -0300

    feat: remove item from cart function

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -4,6 +4,8 @@ const CartContext = React.createContext({
 	items: [],
 	totalAmount: 0,
 	addItem: item => {},
+	removeItem: id => {},
+	clearCart: () => {},
 });
 
 const defaultCartState = {
@@ -47,6 +49,36 @@ export function CartProvider({ children }) {
 		});
 	}
 
+	const removeItemFromCartHandler = id => {
+		const existingItemIndex = cart.items.findIndex(item => item.id === id);
+		const existingItem = cart.items[existingItemIndex];
+
+		const updatedTotalAmount = cart.totalAmount - existingItem.price;
+
+		let updatedItems;
+
+		if (existingItem.amount === 1) {
+			updatedItems = cart.items.filter(item => item.id !== id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...cart.items];
+			updatedItems[existingItemIndex] = updatedItem;
+		}
+
+		localStorage.setItem(
+			"cart",
+			JSON.stringify({
+				items: updatedItems,
+				totalAmount: updatedTotalAmount,
+			})
+		);
+
+		setCart({
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		});
+	};
+
 	function clearCartHandler() {
 		localStorage.removeItem("cart");
 		setCart(defaultCartState);
@@ -56,6 +88,7 @@ export function CartProvider({ children }) {
 		items: cart.items,
 		totalAmount: cart.totalAmount,
 		addItem: addItemToCartHandler,
+		removeItem: removeItemFromCartHandler,
 		clearCart: clearCartHandler,
 	};
 	return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
