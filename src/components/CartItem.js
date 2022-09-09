ee36525f646b1ef82b0723dfc0f821b600740766
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-09-08 21:29:55 -0300

    feat: add responsiveness to Cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,18 +3,20 @@ import styles from "./CartItem.module.css";
 function CartItem({ name, price, amount, image, onRemove, onAdd }) {
 	return (
 		<li className={styles["cart-item"]}>
-			<picture className={styles.image}>
-				<img src={image} alt="" />
-			</picture>
-			<div className={styles.description}>
-				<h2>{name}</h2>
+			<div className={styles["cart-item-info"]}>
+				<picture className={styles.image}>
+					<img src={image} alt="" />
+				</picture>
+				<div className={styles.description}>
+					<h2>{name}</h2>
 
-				<div className={styles.summary}>
-					<span className={styles.price}>${price.toFixed(2)}</span>
-					<span className={styles.amount}>x {amount}</span>
+					<div className={styles.summary}>
+						<span className={styles.price}>${price.toFixed(2)}</span>
+						<span className={styles.amount}>x {amount}</span>
+					</div>
 				</div>
 			</div>
-			<div className={styles.actions}>
+			<div className={styles["cart-item-actions"]}>
 				<button onClick={onAdd}>+</button>
 				<button onClick={onRemove}>−</button>
 			</div>
